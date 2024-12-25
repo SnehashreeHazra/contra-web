627a1739adb52e35fe51ac41376c2ed4cba258b2
Snehashree Hazra <snehashreehazra783@gmail.com>
2024-12-25 13:17:11 +0530

    Add files via upload

--- projects.js
+++ projects.js
@@ -1,4 +1,4 @@
-const navBar = document.querySelector('.nav-bar');
+const navBar = document.querySelector('.navbar-section');
 
 // Listen to scroll event
 window.addEventListener('scroll', () => {
@@ -11,39 +11,3 @@ window.addEventListener('scroll', () => {
   }
 });
 
-
-
-const grid = document.getElementById("grid");
-const links = document.querySelectorAll(".links a");
-
-links.forEach(link => {
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
-    const layout = e.target.dataset.layout;
-
-    switch (layout) {
-      case "all":
-        grid.style.gridTemplateColumns = "repeat(2, 1fr)";
-        grid.style.gridTemplateRows = "repeat(3, 1fr)";
-        break;
-      case "commertial":
-        grid.style.gridTemplateColumns = "1fr 2fr";
-        grid.style.gridTemplateRows = "repeat(2, auto)";
-        break;
-      case "landscape":
-        grid.style.gridTemplateColumns = "repeat(3, 1fr)";
-        grid.style.gridTemplateRows = "auto auto";
-        break;
-      case "interior":
-        grid.style.gridTemplateColumns = "1fr 3fr 1fr";
-        grid.style.gridTemplateRows = "auto";
-        break;
-      case "architecture":
-        grid.style.gridTemplateColumns = "repeat(4, 1fr)";
-        grid.style.gridTemplateRows = "auto auto";
-        break;
-      default:
-        break;
-    }
-  });
-});
